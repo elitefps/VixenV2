93b25618d1d05a16bfe1f3e0b942f3b0f71a3b91
elitefps <141988797+elitefps@users.noreply.github.com>
2024-07-10 18:18:38 +0200

    sirboomalot wants tosend you a message..

--- VixenMain/pagetwo.js
+++ VixenMain/pagetwo.js
@@ -27,19 +27,50 @@ function initNavbarToggle() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-
-    class App extends React.Component {
-      render() {
-        return (
-          <div>
-            <h1>Welcome to Page Two</h1>
-            <p>This is a simple React component integrated into pagetwo.html.</p>
+  class Accordion extends React.Component {
+    render() {
+      return (
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Accordion Item #1
+              </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                This is the first item's accordion body. It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element.
+              </div>
+            </div>
           </div>
-        );
-      }
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingTwo">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Accordion Item #2
+              </button>
+            </h2>
+            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                This is the second item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element.
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingThree">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Accordion Item #3
+              </button>
+            </h2>
+            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                This is the third item's accordion body. It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element.
+              </div>
+            </div>
+          </div>
+        </div>
+      );
     }
-  
-    // Render the React component into the DOM element with id 'root'
-    ReactDOM.render(<App />, document.getElementById('root'));
-  });
-  
+  }
+
+  ReactDOM.render(<Accordion />, document.getElementById('root'));
+});
